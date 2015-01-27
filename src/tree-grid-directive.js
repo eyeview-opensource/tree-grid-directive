1bba52d19941b4d498c9e28fad095def4255c6d8
Omri Shaiko <omri@eyeviewdigital.com>
2015-01-27 12:38:31 -0500

    added advertiser

--- src/tree-grid-directive.js
+++ src/tree-grid-directive.js
@@ -46,7 +46,8 @@
           levelOneAction: '&',
           updateRowStatus: '&',
           openPlacementEndedModal: '&',
-          campaignStatus: '='
+          campaignStatus: '=',
+          advertiserId: '@'
         },
         link: function(scope, element, attrs) {
           var error, expandingProperty, expand_all_parents, expand_level, for_all_ancestors, for_each_branch, get_parent, n, on_treeData_change, select_branch, selected_branch, tree;
